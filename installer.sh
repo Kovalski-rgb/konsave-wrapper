0265bae860cfb9f260f1cad88b8183434c3e2a85
Kovalski <andre.kovalski@tecpuc.com.br>
2023-11-16 08:24:20 -0300

    removing \n on installer script

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@ installDir="~/.config/konsave/"
 if grep -Fq "konsave()" ~/.zshrc ; then
   echo "konsave reference is already on zshrc!"
 else
-  echo -e "\nkonsave() {\n      ${installDir}konsave-wrapper/starter.sh \$@\n}" >> ~/.zshrc
+  echo -e "konsave() {\n      ${installDir}konsave-wrapper/starter.sh \$@\n}" >> ~/.zshrc
 fi
 mkdir -p postHooks/
 mkdir -p hooks/
